f4db65acd23af0c5fb0afbebaba34d015c9060cd
DELMUS1M <ogoradelmus1m@gmail.com>
2023-01-10 19:52:53 +0000

    Correction 100

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -20,4 +20,4 @@ def append_after(filename="", search_string="", new_string=""):
                 res_line += [new_string]
 
     with open(filename, 'w', encoding="utf-8") as f:
-       f.write("".join(res_line))
+        f.write("".join(res_line))
